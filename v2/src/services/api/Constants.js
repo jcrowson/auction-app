6dd0607e8516dbb9d8f53faff454bf771bf2468b
James Crowson <james.crowson@itpeoplecorp.com>
2018-04-20 14:45:49 -0400

    Add Constants file to services

---
+++ v2/src/services/api/Constants.js
@@ -0,0 +1,3 @@
+export const ACCESS_TOKEN = '12345';
+export const API_ENDPOINT = 'https://postman-echo.com/';
+export const HEADERS = new Headers({'content-type': 'application/json', 'cache-control': 'no-cache', 'x-access-token': ACCESS_TOKEN});
--- v2/src/services/api/Users.js
+++ v2/src/services/api/Users.js
@@ -1,31 +1,26 @@
-const ACCESS_TOKEN = '12345';
-const API_ENDPOINT = 'https://postman-echo.com/';
-const GET_REQUEST_OPTIONS = {
-  headers: new Headers({'content-type': 'application/json', 'cache-control': 'no-cache', 'x-access-token': ACCESS_TOKEN}),
-  'method': 'GET',
-};
-const POST_REQUEST_OPTIONS = {
-  headers: new Headers({'content-type': 'application/json', 'cache-control': 'no-cache', 'x-access-token': ACCESS_TOKEN}),
-  'method': 'POST',
-};
+import { API_ENDPOINT, HEADERS, ACCESS_TOKEN } from './Constants.js';
 
 export default class Users {
 
   login(user) {
     return new Promise(function (resolve, reject) {
-      POST_REQUEST_OPTIONS.body = JSON.stringify(user);
-      fetch(API_ENDPOINT + 'post', POST_REQUEST_OPTIONS)
-      .then(response => response.json())
-      .then(data => resolve(data));
+      fetch(API_ENDPOINT + 'post', {
+        headers: HEADERS,
+        method: 'POST',
+        body: JSON.stringify(user),
+      }).then(response => response.json())
+        .then(data => resolve(data));
     });
   }
 
   createNewUser(user) {
     return new Promise(function (resolve, reject) {
-      POST_REQUEST_OPTIONS.body = JSON.stringify(user);
-      fetch(API_ENDPOINT + 'post', POST_REQUEST_OPTIONS)
-      .then(response => response.json())
-      .then(data => resolve(data));
+      fetch(API_ENDPOINT + 'post', {
+        headers: HEADERS,
+        method: 'POST',
+        body: JSON.stringify(user),
+      }).then(response => response.json())
+        .then(data => resolve(data));
     });
   }
 
